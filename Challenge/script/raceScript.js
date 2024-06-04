8663c7c06f238196602800c1b9246a6ce6771b1d
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2024-06-04 10:17:04 -0300

    carrossel de corridas

---
+++ Challenge/script/raceScript.js
@@ -0,0 +1,35 @@
+const corridas = document.querySelectorAll(".corrida .pista")
+let indexCorrida = 0
+let intervaloTempo = null
+
+window.onload=comecaCalendario()
+function comecaCalendario(){
+    if(corridas.length >= 0){
+        corridas[indexCorrida].classList.add("mostraCorrida")
+        intervaloTempo = setInterval(proxCorrida, 5000)
+    }
+}
+function apresentaCorrida(index){
+    if(index >= corridas.length){
+        indexCorrida = 0
+    }
+    else if(index < 0){
+        indexCorrida = corridas.length - 1
+    }
+
+    corridas.forEach(corrida => {
+        corrida.classList.remove("mostraCorrida")
+    })
+    corridas[indexCorrida].classList.add("mostraCorrida")
+}
+
+function antCorrida(){
+    clearInterval(intervaloTempo)
+    indexCorrida--
+    apresentaCorrida(indexCorrida)
+}
+
+function proxCorrida(){
+    indexCorrida++
+    apresentaCorrida(indexCorrida)
+}
